2c87710736cef321539c3d6739db8fc957247a95
MMDemarchiArq <50173122+MMDemarchiArq@users.noreply.github.com>
2019-05-01 10:36:30 -0300

    Teste

---
+++ data.js
@@ -0,0 +1,321 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5510843728900063,
+        "pitch": 0.24907251054311352,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.839156513524248,
+          "pitch": 0.3259189952757602,
+          "rotation": 0,
+          "target": "5-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-p_location_3",
+      "name": "p_Location_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.597412851385151,
+        "pitch": 0.45126078380753576,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-area-gourmet",
+      "name": "Area Gourmet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.7353065711406632,
+        "pitch": 0.4347364918504848,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-quintal",
+      "name": "Quintal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.873932133907843,
+        "pitch": 0.5338300055273795,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-p_location_4",
+      "name": "p_Location_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.415218352168873,
+        "pitch": 0.25493304020295326,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4550694101586803,
+          "pitch": 0.2564204247027071,
+          "rotation": 0,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": -2.767507723331283,
+          "pitch": 0.1696899062942343,
+          "rotation": 0,
+          "target": "6-sala-de-jantar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sala-de-jantar",
+      "name": "Sala de Jantar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.6871613875191613,
+        "pitch": 0.40421282381862156,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6942355488612648,
+          "pitch": 0.23023529220707317,
+          "rotation": 0,
+          "target": "7-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.2079754737695634,
+        "pitch": 0.3459040250248062,
+        "fov": 1.3391310272729973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.443489130726702,
+          "pitch": 0.4853370987180021,
+          "rotation": 12.566370614359176,
+          "target": "2-area-gourmet"
+        },
+        {
+          "yaw": -0.9851459711666273,
+          "pitch": 0.3522857763510423,
+          "rotation": 0,
+          "target": "6-sala-de-jantar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-p_location_9",
+      "name": "p_Location_9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-p_location_10",
+      "name": "p_Location_10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casa do Orlandinho",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
